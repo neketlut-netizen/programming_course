aca9feb565af9c08199954d00fee59bc668a5da2
Bamich <neket.lut@gmail.com>
2026-03-31 18:38:35 +0700

    метод Джордана(исправленная)

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,5 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+
+void GaussJordan(double** matrix, int M, int N) {
+    int Row = 0;
+    for (int col = 0; col < N && Row < M; ++col) {
+        int Swap_Row = Row;
+        for (int i = Row + 1; i < M; ++i) {
+            if (fabs(matrix[i][col]) > fabs(matrix[Swap_Row][col])) {
+                Swap_Row = i;
+            }
+        }
+
+        double* temp = matrix[Row];
+        matrix[Row] = matrix[Swap_Row];
+        matrix[Swap_Row] = temp;
+
+        double main_val = matrix[Row][col];
+
+        for (int i = col; i <= N; ++i) {
+            matrix[Row][i] /= main_val;
+        }
+
+        for (int i = 0; i < M; ++i) {
+            if (i != Row) {
+                double diffrent = matrix[i][col];
+                for (int j = col; j <= N; ++j) {
+                    matrix[i][j] -= diffrent * matrix[Row][j];
+                }
+            }
+        }
+        Row++;
+    }
+}
+
+
 
 int main() {
     int M, N;
@@ -10,12 +46,11 @@ int main() {
         return 0;
     }
     
-    if (fscanf(file, "%d %d", &M, &N) != 2) {
+    if (fscanf(file, "%d %d", &M, &N) != 2) { // M и N
         fclose(file);
         return 0;
     }
 
-    std::cout << M << " " << N << "\n"; //_____________________________________незабудь убрать______________________
 
 
     //создание двойного массива и заполнение (lf - double)
@@ -30,13 +65,9 @@ int main() {
     }
     fclose(file);
 
+    GaussJordan(matrix, M, N);
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j <= N; j++) {
-            printf("%lf ", matrix[i][j]);
-        }
-        printf("\n");
-    }
+    
     
 
 
