9acc1337bcf58449c12dddeccb3d1b39c2a727fe
Bamich <neket.lut@gmail.com>
2026-05-04 20:14:17 +0700

    изменил 1 задание полностью + добавил LSD

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -3,33 +3,26 @@
 #include <cstdio>
 
 int main() {
+    // ================================================================================================================================================
     FILE* file = fopen("input.txt", "r");
     if (file == NULL) {
-        printf("ERROR with FILE\n");
+        printf("ERROR with input.txt\n");
         return 0;
     }
 
-    int RowMax=0;
-    int ColMax = 0;
-    int N = 0; //Длинна квадратной матрицы
+    int N = 0; 
     int* Row = new int[100];
     int* Col = new int[100];
     int count = 0;
 
-    while (count < 100 && fscanf(file, "%d %d", &Row[count], &Col[count]) == 2) count++;
+    
+    while (count < 100 && fscanf(file, "%d %d", &Row[count], &Col[count]) == 2) {
+        if (Row[count] > N) N = Row[count];
+        if (Col[count] > N) N = Col[count];
+        count++;
+    }
     fclose(file);
 
-    ///максимальный элемент в массиве
-    for (int i = 0; i < count; i++) {
-        if (RowMax < Row[i]) RowMax = Row[i];
-    }
-    for (int i = 0; i < count; i++) {
-        if (ColMax < Col[i]) ColMax = Col[i];
-    }
-    if (ColMax > RowMax) N = ColMax;
-    else N = RowMax;
-    
-    //Матрица смежности
     int size = N + 1;
     int** matrix = new int* [size];
     for (int i = 0; i < size; i++) {
@@ -37,81 +30,104 @@ int main() {
         for (int j = 0; j < size; j++) matrix[i][j] = 0;
     }
 
-    for (int i = 0; i < size; i++) {
-        matrix[0][i] = i;
-        matrix[i][0] = i;
-    }
-    for (int i = 0; i < count; i++) {
-        matrix[Row[i]][Col[i]] = 1;
-        matrix[Col[i]][Row[i]] = 1;
-    }
-
-    file = fopen("output1.txt", "w");
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            fprintf(file,"%d ", matrix[i][j]);
+    
+        for (int i = 0; i < count; i++) {
+            matrix[Row[i]][Col[i]] = 1;
+            matrix[Col[i]][Row[i]] = 1;
         }
-        fprintf(file,"\n");
+
+    FILE* fileOut1 = fopen("output1.txt", "w");
+    fprintf(fileOut1, "%d\n", N);
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            fprintf(fileOut1, "%d ", matrix[i][j]);
+        }
+        fprintf(fileOut1, "\n");
     }
-    fclose(file);
+    fclose(fileOut1);
+
     for (int i = 0; i < size; i++) delete[] matrix[i];
-    delete[] matrix; delete[] Row; delete[] Col; count = 0; N = 0; RowMax = 0; ColMax = 0; size = 0;
-    //__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-    FILE* FileOutput = fopen("output2.txt", "w"); 
-    file = fopen("output1.txt", "r");
-    int MaxSize = 0;
-    size = -1;
+    delete[] matrix; delete[] Row; delete[] Col;
 
-    while (fscanf(file, "%d", &MaxSize) == 1) {
-        if (MaxSize > size) size = MaxSize;
-    }
 
-    fclose(file);
-    file = fopen("output1.txt", "r");
+    // ___________________________________________________________________________________________________________________________________________________
+    FILE* fileIn2 = fopen("output1.txt", "r");
+    
+    fscanf(fileIn2, "%d", &N); 
 
-    matrix = new int* [size + 1];
-    for (int i = 0; i < size + 1; i++) {
-        matrix[i] = new int[size + 1];
-        for (int j = 0; j < size + 1; j++) {
-            fscanf(file, "%d", &matrix[i][j]);
+    matrix = new int* [N + 1];
+    for (int i = 1; i <= N; i++) {
+        matrix[i] = new int[N + 1];
+        for (int j = 1; j <= N; j++) {
+            fscanf(fileIn2, "%d", &matrix[i][j]);
         }
     }
+    fclose(fileIn2);
 
+    int* L = new int[N + 1]; // Кол-во инцидентных дуг с вершиной
+    int* S = new int[N + 1]; // Индексы начала
+    for (int j = 1; j <= N; j++) L[j] = 0;
 
-    for (int i = 1; i < size + 1; i++) {
-        for (int j = 1; j < size + 1; j++) {
+    
+    int Edge = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
             if (matrix[i][j] == 1) {
-                fprintf(FileOutput, "%d ", j);
-            }
-        }
-        fprintf(FileOutput, "\n");
-    }
-
-    for (int i = 0; i < size; i++) delete[] matrix[i];
-    fclose(file); fclose(FileOutput); size = 0; MaxSize = 0;
-    //____________________________________________________________________________________________________________________________________
-    file = fopen("output2.txt", "r");
-    FileOutput = fopen("output3.txt", "w");
-    RowMax = 1; //номер тякущей строки
-    int neighbor;
-    char c;
-
-    while (fscanf(file, "%d", &neighbor) == 1) {
-        if (RowMax < neighbor) {
-            fprintf(FileOutput, "%d %d\n", RowMax, neighbor);
-        }
-        while (fscanf(file, "%c", &c) == 1) {
-            if (c == '\n') {
-                RowMax++;
-                break;
-            }
-            if (c != ' ' && c != '\r') {
-                ungetc(c, file);//вернуть обратно
-                break;
+                L[i]++;
+                Edge++;
             }
         }
     }
 
-    fclose(file); fclose(FileOutput);
+    S[1] = 0;
+    for (int j = 2; j <= N; j++) {
+        S[j] = S[j - 1] + L[j - 1];
+    }
+
+    int* D = new int[Edge];
+    int* U = new int[N + 1]; // Дополнительный массив, дублирующий индексы
+        for (int j = 1; j <= N; j++) U[j] = S[j];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (matrix[i][j] == 1) {
+                    D[U[i]] = j;
+                    U[i]++;
+                }
+            }
+        }
+
+    FILE* fileOut2 = fopen("output2.txt", "w");
+    fprintf(fileOut2, "%d %d\n", N, Edge);
+    for (int i = 1; i <= N; i++) fprintf(fileOut2, "%d ", L[i]); fprintf(fileOut2, "\n");
+    for (int i = 1; i <= N; i++) fprintf(fileOut2, "%d ", S[i]); fprintf(fileOut2, "\n");
+    for (int i = 0; i < Edge; i++) fprintf(fileOut2, "%d ", D[i]); fprintf(fileOut2, "\n");
+    fclose(fileOut2);
+
+    for (int i = 1; i <= N; i++) delete[] matrix[i];
+    delete[] matrix; delete[] U;
+
+
+    // ___________________________________________________________________________________________________________________________________________________
+    FILE* fileIn3 = fopen("output2.txt", "r");
+
+    fscanf(fileIn3, "%d %d", &N, &Edge);
+    for (int i = 1; i <= N; i++) fscanf(fileIn3, "%d", &L[i]);
+    for (int i = 1; i <= N; i++) fscanf(fileIn3, "%d", &S[i]);
+    for (int i = 0; i < Edge; i++) fscanf(fileIn3, "%d", &D[i]);
+    fclose(fileIn3);
+
+    FILE* fileOut3 = fopen("output3.txt", "w");
+
+    for (int i = 1; i <= N; i++) {
+            for (int k = S[i]; k < S[i] + L[i]; k++) {
+                int j = D[k];
+                if (i < j) {
+                    fprintf(fileOut3, "%d %d\n", i, j);
+                }
+            }
+    }
+    fclose(fileOut3);
+    delete[] L; delete[] S; delete[] D;
     return 0;
 }
